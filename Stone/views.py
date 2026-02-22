33bfc86f62744dd2b73c7b6834e70f6031b212bd
Webis <akobyanrobert8@gmail.com>
2026-02-22 20:40:51 +0300

    обновление представления

--- Stone/views.py
+++ Stone/views.py
@@ -1,91 +1,147 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseForbidden, HttpResponseBadRequest, JsonResponse
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, JsonResponse
 from django.core.paginator import Paginator
-from django.core.serializers.json import DjangoJSONEncoder
-
-from django.template.response import TemplateResponse
-
 from django.contrib import messages
-
-from django.db.models import Q, Avg
+from django.db.models import Q, Avg, Count
 from datetime import datetime
+from django.views.decorators.http import require_POST
 
-from .forms import UserForm, ProductForm
-from .models import Feedback, ByModels
+from .forms import UserForm, CountertopOrderForm
+from .models import Feedback, Stone, StoneCategory, CountertopOrder
 
 def index(request):
-    header = "Данные пользователя"
-    langs = ["Python", "JS", "Django", "FastAPI"]
-    user = {"name":"Ilya", "age":33}
-    host = request.META.get('HTTP_HOST', 'неизвестный хост')
-    method = request.method
-    path = request.path
-    user_agent = request.META.get('HTTP_USER_AGENT', 'неизвестный браузер')
-    ip = request.META.get('REMOTE_ADDR', 'неизвестный IP')
+    """Главная страница"""
+    popular_stones = Stone.objects.filter(is_popular=True, in_stock=True)[:6]
+    new_stones = Stone.objects.filter(is_new=True, in_stock=True)[:4]
+    categories = StoneCategory.objects.all()
+    recent_reviews = Feedback.objects.filter(request_type='review', is_published=True)[:3]
+    
     context = {
-        "header": header,
-        "langs": langs,
-        "name": user["name"], 
-        "age": user["age"],
-        "host": host,
-        "method":method,
-        "user_agent":user_agent,
-        "ip":ip,
-        "path":path,
-        "my_date": datetime.now()}
-    return render (request, "index.html", context=context)
-#     return HttpResponse(f'''
-#     <p>Host: {host}</p>
-#     <p>Path: {path}</p>
-#     <p>User_agent:{user_agent}</p>
-#     <p>IP:{ip}</p>
-#     <p>Method:{method}</p>
-# ''')
+        'popular_stones': popular_stones,
+        'new_stones': new_stones,
+        'categories': categories,
+        'recent_reviews': recent_reviews,
+        'my_date': datetime.now(),
+    }
+    return render(request, "index.html", context)
+
+def catalog(request):
+    """Каталог камней"""
+    category_slug = request.GET.get('category')
+    min_price = request.GET.get('min_price')
+    max_price = request.GET.get('max_price')
+    hardness = request.GET.get('hardness')
+    search = request.GET.get('search')
+    sort = request.GET.get('sort', '-created_at')
+    
+    stones = Stone.objects.filter(in_stock=True)
+    
+    if category_slug:
+        stones = stones.filter(category__slug=category_slug)
+    
+    if min_price:
+        stones = stones.filter(price_per_sqm__gte=min_price)
+    
+    if max_price:
+        stones = stones.filter(price_per_sqm__lte=max_price)
+    
+    if hardness:
+        stones = stones.filter(hardness__gte=hardness)
+    
+    if search:
+        stones = stones.filter(
+            Q(name__icontains=search) | 
+            Q(description__icontains=search)
+        )
+    
+    if sort == 'price_asc':
+        stones = stones.order_by('price_per_sqm')
+    elif sort == 'price_desc':
+        stones = stones.order_by('-price_per_sqm')
+    elif sort == 'name_asc':
+        stones = stones.order_by('name')
+    elif sort == 'popular':
+        stones = stones.annotate(order_count=Count('countertoporder')).order_by('-order_count')
+    else:
+        stones = stones.order_by('-created_at')
+    
+    paginator = Paginator(stones, 12)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    categories = StoneCategory.objects.all()
+    
+    context = {
+        'page_obj': page_obj,
+        'categories': categories,
+        'current_category': category_slug,
+        'min_price': min_price,
+        'max_price': max_price,
+        'hardness': hardness,
+        'search': search,
+        'sort': sort,
+    }
+    return render(request, "catalog.html", context)
+
+def stone_detail(request, slug):
+    """Детальная страница камня"""
+    stone = get_object_or_404(Stone, slug=slug, in_stock=True)
+    
+    similar_stones = Stone.objects.filter(
+        category=stone.category, 
+        in_stock=True
+    ).exclude(id=stone.id)[:4]
+    
+    if request.method == 'POST':
+        form = CountertopOrderForm(request.POST)
+        if form.is_valid():
+            order = form.save(commit=False)
+            order.stone = stone
+            order.save()
+            messages.success(request, f'Спасибо, {order.customer_name}! Ваш заказ принят. Наш менеджер свяжется с вами в ближайшее время.')
+            return redirect('order_success', order_id=order.id)
+    else:
+        form = CountertopOrderForm(initial={'stone': stone})
+        form.fields['stone'].widget = forms.HiddenInput()
+    
+    context = {
+        'stone': stone,
+        'similar_stones': similar_stones,
+        'form': form,
+    }
+    return render(request, "stone_detail.html", context)
+
+def order_success(request, order_id):
+    """Страница успешного заказа"""
+    order = get_object_or_404(CountertopOrder, id=order_id)
+    context = {
+        'order': order,
+        'area': order.calculate_area(),
+        'price': order.calculate_price(),
+    }
+    return render(request, "order_success.html", context)
 
 def by_product(request):
+    """Быстрый заказ столешницы"""
     if request.method == "POST":
-        form = ProductForm(request.POST)
+        form = CountertopOrderForm(request.POST)
         if form.is_valid():
-            ByModels.objects.create(
-            name=form.cleaned_data['name'],
-            numTel=form.cleaned_data['numTel'],
-            adress=form.cleaned_data['adress'] or "Самовывоз",
-            request_by=form.cleaned_data['langs']
-            )
-            context = {
-                'name':form.cleaned_data['name'],
-                'numTel':form.cleaned_data['numTel'],
-                'adress':form.cleaned_data['adress'] or 'Самовывоз',
-                'langs':form.cleaned_data['langs'],
-            }
-            return render(request, "order_success.html", context)
-        else:
-            return render(request, "by_product.html",{'form':form})
+            order = form.save()
+            messages.success(request, f'Спасибо, {order.customer_name}! Ваш заказ принят.')
+            return redirect('order_success', order_id=order.id)
     else:
-        form = ProductForm()
-        return render(request,"by_product.html", {'form':form})
-
-def galerey(request):
-    return render(request, "galerey.html")
-
-def gallery(request):
-    return render(request, "gallery.html")
-
-def about(request):
-    return render (request, "about.html" )
-
-def contact(request):
-    return render (request,"contact.html" )
-
-def products(request, id):
-    return HttpResponse(f"Виды изделий:{id}")
-
-def comments(request, id):
-    return HttpResponse(f"Комментарии:{id}")
+        form = CountertopOrderForm()
+    
+    stones = Stone.objects.filter(in_stock=True)
+    
+    context = {
+        'form': form,
+        'stones': stones,
+    }
+    return render(request, "by_product.html", context)
 
 def questions(request):
+    """Страница отзывов и вопросов"""
     if request.method == "POST":
         form = UserForm(request.POST)
         if form.is_valid():
@@ -97,72 +153,45 @@ def questions(request):
             rating = form.cleaned_data.get('rating')
 
             Feedback.objects.create(
-                request_type = langs,
-                name = name,
-                numTel = numTel,
-                adress = adress,
-                message = message,
-                rating = rating,
+                request_type=langs,
+                name=name,
+                numTel=numTel,
+                adress=adress,
+                message=message,
+                rating=rating,
             )
 
-            context = {
-                'form': form,
-                'submitted': True,
-                'langs': langs,
-                'name': name,
-                'numTel': numTel,
-                'adress': adress,
-                'message': message,
-                'rating': rating,
-            }
             messages.success(request, f'Спасибо, {name}! Ваше обращение принято.')
-            return render(request,"questions.html", context)
+            return redirect('questions')
     else:
         form = UserForm()
 
-    filter_type = request.GET.get('type','all')
-    filter_rating = request.GET.get('rating','all')
-    sort_by = request.GET.get('sort','-created_at')
-    search_query = request.GET.get('search','')
+    filter_type = request.GET.get('type', 'all')
+    filter_rating = request.GET.get('rating', 'all')
+    search_query = request.GET.get('search', '')
 
-    feedbacks = Feedback.objects.all()
+    feedbacks = Feedback.objects.filter(is_published=True)
 
     if filter_type != 'all':
         feedbacks = feedbacks.filter(request_type=filter_type)
     
-    if filter_rating != 'all' and filter_rating.isdigit():  
+    if filter_rating != 'all' and filter_rating.isdigit():
         feedbacks = feedbacks.filter(rating=int(filter_rating))
     
     if search_query:
         feedbacks = feedbacks.filter(
             Q(name__icontains=search_query) |
-            Q(message__icontains=search_query)  
+            Q(message__icontains=search_query)
         )
     
-    if sort_by == 'reting_asc':
-        feedbacks = feedbacks.order_by('rating')
-    elif sort_by == 'rating_desc':
-        feedbacks = feedbacks.order_by('-rating')
-    elif sort_by == 'name_asc':
-        feedbacks = feedbacks.order_by('name')
-    elif sort_by == 'name_desc':
-        feedbacks = feedbacks.order_by('-name')
-    elif sort_by == 'oldest':
-        feedbacks = feedbacks.order_by('created_at')
-    else:
-        feedbacks = feedbacks.order_by('-created_at')
+    feedbacks = feedbacks.order_by('-created_at')
     
-
     total_count = Feedback.objects.count()
-
     avg_result = Feedback.objects.filter(
         request_type='review',
         rating__isnull=False
     ).aggregate(Avg('rating'))
     avg_rating = avg_result['rating__avg']
-
-
-# ПАГИНАЦИЯ
 
     paginator = Paginator(feedbacks, 10)
     page_number = request.GET.get('page')
@@ -170,42 +199,56 @@ def questions(request):
 
     context = {
         'form': form,
-        'submitted': False,
         'page_obj': page_obj,
-        'feedbacks': page_obj,
         'filter_type': filter_type,
         'filter_rating': filter_rating,
-        'sort_by': sort_by,
         'search_query': search_query,
         'total_count': total_count,
         'avg_rating': avg_rating,
     }
+    return render(request, "questions.html", context)
 
-    return render (request, "questions.html", context)
+def gallery(request):
+    """Галерея работ"""
+    stones = Stone.objects.filter(in_stock=True)
+    
+    category_id = request.GET.get('category')
+    if category_id:
+        stones = stones.filter(category_id=category_id)
+    
+    categories = StoneCategory.objects.all()
+    
+    context = {
+        'stones': stones,
+        'categories': categories,
+    }
+    return render(request, "gallery.html", context)
 
-def new(request):
-    return HttpResponse("New")
+def about(request):
+    """Страница о нас"""
+    return render(request, "about.html")
 
-def top(request):
-    return HttpResponse("Top")
+def contact(request):
+    """Страница контактов"""
+    return render(request, "contact.html")
 
-def user(request):
-    name = request.GET.get("name", "Неопределено")
-    age = request.GET.get("age", 0)
-    return HttpResponse(f"<h2>Имя:{name} Возраст:{age}</h2>")
-
-############################
-class Person:
-    def __init__(self, name, age):
-        self.name = name 
-        self.age = age
-class PersonEncoder(DjangoJSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, Person):
-            return {"name":obj.name, "age":obj.age}
-        return super().default(obj)
-
-def user_response(request):
-    ilya = Person("Ilya", 33)
-    return JsonResponse (ilya, safe=False, encoder=PersonEncoder)
-#################################
+@require_POST
+def calculate_price(request):
+    """API для расчета цены"""
+    stone_id = request.POST.get('stone_id')
+    length = float(request.POST.get('length', 0))
+    width = float(request.POST.get('width', 0))
+    
+    try:
+        stone = Stone.objects.get(id=stone_id)
+        area = length * width
+        price = area * float(stone.price_per_sqm)
+        
+        return JsonResponse({
+            'success': True,
+            'area': round(area, 2),
+            'price': round(price, 2),
+            'price_per_sqm': float(stone.price_per_sqm)
+        })
+    except (Stone.DoesNotExist, ValueError, TypeError):
+        return JsonResponse({'success': False, 'error': 'Ошибка расчета'})
