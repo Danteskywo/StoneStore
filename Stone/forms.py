5f0fa81f2aeb55a2f59dac8a6f6462727ed631c7
Webis <akobyanrobert8@gmail.com>
2026-02-22 20:37:31 +0300

    обновление формы

--- Stone/forms.py
+++ Stone/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Feedback, CountertopOrder, Stone
 
 CHOICE_TYPE = [
     ("review", "Отзыв"),
@@ -9,17 +10,17 @@ class UserForm(forms.Form):
     name = forms.CharField(
         label="Ваше Имя",
         max_length=30,
-        widget=forms.TextInput()
+        widget=forms.TextInput(attrs={'placeholder': 'Введите ваше имя'})
     )
     numTel = forms.CharField(
         label='Номер телефона',
         max_length=20,
-        widget=forms.TextInput()
+        widget=forms.TextInput(attrs={'placeholder': '+7 (___) ___-__-__'})
     )
     adress = forms.CharField(
         label='Адрес',
         required=False,
-        widget=forms.Textarea()
+        widget=forms.Textarea(attrs={'rows': 2, 'placeholder': 'Ваш адрес (необязательно)'})
     )
     langs = forms.ChoiceField(
         choices=CHOICE_TYPE,
@@ -29,41 +30,46 @@ class UserForm(forms.Form):
     )
     message = forms.CharField(
         label="Ваше сообщение",
-        widget=forms.Textarea()
+        widget=forms.Textarea(attrs={'rows': 4, 'placeholder': 'Напишите ваш отзыв или вопрос'})
     )
     rating = forms.ChoiceField(
-        choices=[(1, '★'), (2, '★★'), (3, '★★★'), (4, '★★★★'), (5, '★★★★★')],
+        choices=[(5, '★★★★★'), (4, '★★★★'), (3, '★★★'), (2, '★★'), (1, '★')],
         label="Оценка",
         required=False,
-        widget=forms.RadioSelect(attrs={'class':'form-control'})
+        widget=forms.RadioSelect()
     )
 
-CHOICE_PHONE = [
-    ("site","Оставить на сайте"),
-    ("call","Позвонить"),
-]
-
-class ProductForm(forms.Form):
-    name = forms.CharField(
-        label="Ваше Имя",
-        max_length=30,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    numTel = forms.CharField(
-        label='Номер телефона',
-        max_length=20,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    adress = forms.CharField(
-        label='Адрес',
-        required=False,
-        widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 3})
-    )
-    langs = forms.ChoiceField(
-        choices = CHOICE_PHONE,
-        label="Тип обращения",
-        widget=forms.RadioSelect,
-        initial="call"
-    )
-
-
+class CountertopOrderForm(forms.ModelForm):
+    class Meta:
+        model = CountertopOrder
+        fields = ['stone', 'length', 'width', 'thickness', 'edge_type', 'sink_type', 
+                  'cutouts', 'customer_name', 'customer_phone', 'customer_email', 
+                  'customer_address', 'comment']
+        widgets = {
+            'stone': forms.Select(attrs={'class': 'form-select'}),
+            'length': forms.NumberInput(attrs={'class': 'form-control', 'step': '0.01', 'min': '0.5'}),
+            'width': forms.NumberInput(attrs={'class': 'form-control', 'step': '0.01', 'min': '0.5'}),
+            'thickness': forms.Select(attrs={'class': 'form-select'}),
+            'edge_type': forms.Select(attrs={'class': 'form-select'}),
+            'sink_type': forms.Select(attrs={'class': 'form-select'}),
+            'cutouts': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'customer_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Иванов Иван'}),
+            'customer_phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '+7 (999) 123-45-67'}),
+            'customer_email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'email@example.com'}),
+            'customer_address': forms.Textarea(attrs={'class': 'form-control', 'rows': 2}),
+            'comment': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }
+        labels = {
+            'stone': 'Выберите камень',
+            'length': 'Длина (м)',
+            'width': 'Ширина (м)',
+            'thickness': 'Толщина (мм)',
+            'edge_type': 'Тип кромки',
+            'sink_type': 'Тип мойки',
+            'cutouts': 'Вырезы',
+            'customer_name': 'Ваше имя',
+            'customer_phone': 'Телефон',
+            'customer_email': 'Email',
+            'customer_address': 'Адрес доставки',
+            'comment': 'Комментарий к заказу',
+        }
